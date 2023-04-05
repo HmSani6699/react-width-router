2456c574c1344ed6e97189f5937f49d5d178a9d7
Hm Sani <hmsadikurrahman120@gmail.com>
2023-04-05 06:59:25 +0600

    ad to error message

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Contact from "./component/Contact/Contact";
+import About from "./component/About/About";
+import Error from "./component/Error/Error";
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    errorElement:<Error></Error>,
+  },
+  {
+    path: "/contact",
+    element: <Contact></Contact>,
+    errorElement:<Error></Error>,
+  },
+  {
+    path: "/about",
+    element: <About></About>,
+  },
+]);
 
-const router=createBrowserRouter([
-{
-  path:'/',
-  element:<App></App>,
-}
-])
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-  <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
