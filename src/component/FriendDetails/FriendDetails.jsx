572d60cef5e2c1fbbf0cde9b7681e2310d8f4e89
Hm Sani <hmsadikurrahman120@gmail.com>
2023-04-05 09:23:05 +0600

    display to friend detail

--- src/component/FriendDetails/FriendDetails.jsx
+++ src/component/FriendDetails/FriendDetails.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const FriendDetails = () => {
+    const friend=useLoaderData();
+    const {id,name,phone,email}=friend;
     return (
         <div>
-            <h2>Friends Details</h2>
+            <h5>ID: {id}</h5>
+            <h4>{name}</h4>
+            <p><small>Email: {email}</small></p>
+            <p><small>Phone: {phone}</small></p>
         </div>
     );
 };
